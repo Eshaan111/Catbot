6a15863b79dcc891a6eb516858f466520d620f53
Eshaan111 <eshaanbansal2005@gmail.com>
2025-10-29 21:39:50 +0530

    added first api call to tenor api

--- routes/api.js
+++ routes/api.js
@@ -5,17 +5,40 @@ const api_key = config.apiKey;
 
 apiRouter.use(express.json());
 
-console.log('API KEY FROM API.js', api_key);
+// console.log('API KEY FROM API.js', api_key);
+
+// apiRouter.get('/', async (req, res) => {
+//     console.log('Api Route Get Request ')
+//     query = req.query  // RETURNS JS OBJECT 
+//     phrase = query.phrase;
+//     console.log(`queries = `, query)
+//     console.log('phrase = ', phrase)
+//
+//     const response = await fetch(`https://tenor.googleapis.com/v2/search?q=${phrase}&key=${api_key}&client_key=my_test_app&limit=8`)
+//     if (!response.ok) {
+//         throw new Error('Tenor Api Error', response)
+//
+//     }
+//     const data = await response.json()
+//     console.log('tenor response', data)
+//
+//     res.json({
+//         message: 'success',
+//         data: query,
+//     });
+//     // res.status(404).send('not-found');
+// })
 
 apiRouter.get('/', (req, res) => {
-    console.log('Api Route Get Request ')
-    query = req.query  // RETURNS JS OBJECT 
-    console.log(`queries = `,query)
-    res.json({
-        message: 'success',
-        data: query,
-    });
-    // res.status(404).send('not-found');
+
+    query = req.query;
+    phrase = query.phrase;
+
+    fetch(`https://tenor.googleapis.com/v2/search?q=${phrase}&key=${api_key}&client_key=my_test_app&limit=8`)
+        .then(resolve => { return resolve.json() })
+        .then(data => console.log(data))
+
 })
+
 
 module.exports = apiRouter;
